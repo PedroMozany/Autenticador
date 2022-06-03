23b7eb49ff866cae1789931954a63a3e54132db5
PedroMozany <pedro.mozanny@hotmail.com>
2022-06-03 18:49:50 -0300

    Programa bruto

---
+++ src/App.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        try {
+            menu();
+        } catch (IllegalStateException ig) {
+            System.out.println("Usuário e/ou senha incorretos.");
+        }
+
+    }
+
+
+    public static void menu() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1 - Cadastrar");
+        System.out.println("2 - Login");
+        int opcao = sc.nextInt();
+        switch (opcao) {
+            case 1:
+                cadastrar();
+            case 2:
+                login();
+            default:
+        }
+    }
+
+
+    public static void cadastrar() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o seu nome: ");
+        String nome = sc.nextLine().toUpperCase();
+        System.out.println("Digite uma senha: ");
+        String senha = sc.nextLine();
+        Sistema sistema = new Sistema(new Usuario(nome, senha));
+        sistema.salvaBaseDados();
+        System.out.println("Cadastrado com Sucesso");
+        menu();
+    }
+
+
+    public static void login() {
+        Sistema sistema = new Sistema();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o seu nome: ");
+        String nome = sc.nextLine().toUpperCase();
+        System.out.println("Digite uma senha: ");
+        String senha = sc.nextLine();
+        Usuario usuario = sistema.buscarUsuario(nome, senha);
+        Periodo periodo = sistema.intervaloHorario();
+        switch (periodo) {
+            case MANHA: System.out.println("Bom dia " + usuario.getUsuario() + ", você se logou ao nosso sistema." + sistema.toString());break;
+            case TARDE: System.out.println("Boa tarde " + usuario.getUsuario() + ", você se logou ao nosso sistema." + sistema.toString());break;
+            case NOITE: System.out.println("Boa noite " + usuario.getUsuario() + ", você se logou ao nosso sistema." + sistema.toString());break;
+            case MADRUGADA: System.out.println("Boa madrugada " + usuario.getUsuario() + ", você se logou ao nosso sistema." + sistema.toString());break;
+            default:
+        }
+    }
+
+
+
+
+}
---
+++ src/Sistema.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Sistema {
+
+    private LinkedList<Usuario> listUsuarios;
+    private LocalDateTime horarioAtual;
+    private int horas;
+    private int minutos;
+
+
+    public Sistema(Usuario usuario) {
+        this.listUsuarios = new LinkedList<>();
+        addUsuario(usuario);
+        this.horarioAtual = LocalDateTime.now();
+        this.horas = horarioAtual.getHour();
+        this.minutos = horarioAtual.getMinute();
+    }
+
+
+    public Sistema() {
+        this.listUsuarios = lerBaseDados("baseDados.txt");
+        this.horarioAtual = LocalDateTime.now();
+        this.horas = horarioAtual.getHour();
+        this.minutos = horarioAtual.getMinute();
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void addUsuario(Usuario usuario) {
+        this.listUsuarios.add(usuario);
+    }
+
+
+    public Usuario buscarUsuario(String nome,String senha) {
+        return this.listUsuarios.stream()
+                .filter(e -> e.getUsuario().equals(nome ) && e.getSenha().equals(senha))
+                .findAny()
+                .orElseThrow(IllegalStateException::new);
+    }
+
+
+    public String toString() {
+        return "\n" + "Você logou as " +
+                this.horas + ":" +
+                this.minutos;
+    }
+
+
+    public Periodo intervaloHorario() {
+        if (getHoras() >= 6 && getHoras() < 12) {
+            return Periodo.MANHA;
+        } else if (getHoras() >= 12 && getHoras() <= 17) {
+            return Periodo.TARDE;
+        } else if (getHoras() > 17 && getHoras() <= 23) {
+            return Periodo.NOITE;
+        } else {
+            return Periodo.MADRUGADA;
+        }
+    }
+
+
+    public LinkedList<Usuario> lerBaseDados(String arquivo) {
+        LinkedList<Usuario> conjuntoUsuarios = new LinkedList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
+            String linha = br.readLine();
+            while (linha != null) {
+                Scanner sc = new Scanner(linha);
+                sc.useDelimiter(",");
+                conjuntoUsuarios.add(new Usuario(sc.next(), sc.next()));
+                linha = br.readLine();
+                sc.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao encontra o arquivo");
+        }
+
+        return conjuntoUsuarios;
+    }
+
+
+
+    public void salvaBaseDados() {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("baseDados.txt", true))) {
+            for (Usuario usuario : listUsuarios) {
+                bw.write(usuario.getUsuario());
+                bw.write(",");
+                bw.write(usuario.getSenha());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao encontra o arquivo");
+        }
+    }
+
+}
